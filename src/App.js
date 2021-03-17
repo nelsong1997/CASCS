94285c5a7ea4aebc5bbe6a46b74901f5ca7c3df3
Gabe N <nelsong1997@gmail.com>
2021-03-16 21:13:03 -0400

    react commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			property: "value"
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<p>sup</p>
+			</div>
+		)
+	}
+}
+
+export default App;
